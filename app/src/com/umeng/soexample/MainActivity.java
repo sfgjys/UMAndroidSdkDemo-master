92e926c2196426d1f2f5f30126d9357abb9672e3
ZHQ <sfgjys@sina.com>
2018-01-06 10:48:01 +0800

    移除了除分享功能外的所有模块

--- app/src/com/umeng/soexample/MainActivity.java
+++ app/src/com/umeng/soexample/MainActivity.java
@@ -7,9 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.umeng.analytics.AnalyticsMainActivity;
 import com.umeng.soexample.share.HomeActivity;
-import com.umeng.error.ErrorActivity;
+
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private static final String TAG = "MainActivity";
@@ -47,20 +46,14 @@ public class MainActivity extends Activity implements View.OnClickListener {
         int id = view.getId();
         switch (id) {
             case R.id.um_analytics_entrance:
-                Intent intentCommon = new Intent(MainActivity.this, AnalyticsMainActivity.class);
-                MainActivity.this.startActivity(intentCommon);
                 break;
             case R.id.um_push_entrance:
-                Intent intentAnalytics = new Intent(MainActivity.this, com.umeng.message.example.SplashTestActivity.class);
-                MainActivity.this.startActivity(intentAnalytics);
                 break;
             case R.id.um_share_entrance:
                 Intent intentShare = new Intent(MainActivity.this, HomeActivity.class);
                 MainActivity.this.startActivity(intentShare);
                 break;
             case R.id.um_error_entrance:
-                Intent intentError = new Intent(MainActivity.this, ErrorActivity.class);
-                MainActivity.this.startActivity(intentError);
                 break;
         }
     }
